d783f4cf3f27ca6c80fa886a29250d8e58c10970
mishrajiiii <93723618+mishrajiiii@users.noreply.github.com>
2021-11-19 19:51:40 +0530

    Create employee.c
    developed by harsh mishra

---
+++ employee.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+struct employee
+{
+    int phno,age,salary;
+    char name[250];
+}emp[20];
+ 
+void main()
+{
+    int i;
+    printf("Enter employee info as name , age ,phone number, salary\n");
+    for(i=0;i<1;i++)
+    {
+        scanf("%s %d %d %d",emp[i].name,&emp[i].age,&emp[i].phno,&emp[i].salary);
+    }
+    printf("\nNAME\t\tAGE\t\tPHONE_NUMBER\t\tSALARY\n");
+    for(i=0;i<1;i++)
+    {
+        printf("%s\t\t%d\t\t%d\t\t\t%d\n",emp[i].name,emp[i].age,emp[i].phno,emp[i].salary);
+    }
+}
