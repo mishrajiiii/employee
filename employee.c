0738f5e243bdea93854e233d0535638d9400a8dd
mishrajiiii <93723618+mishrajiiii@users.noreply.github.com>
2021-11-19 19:53:21 +0530

    Update employee.c

--- employee.c
+++ employee.c
@@ -9,12 +9,12 @@ void main()
 {
     int i;
     printf("Enter employee info as name , age ,phone number, salary\n");
-    for(i=0;i<1;i++)
+    for(i=0;i<20;i++)
     {
         scanf("%s %d %d %d",emp[i].name,&emp[i].age,&emp[i].phno,&emp[i].salary);
     }
     printf("\nNAME\t\tAGE\t\tPHONE_NUMBER\t\tSALARY\n");
-    for(i=0;i<1;i++)
+    for(i=0;i<20;i++)
     {
         printf("%s\t\t%d\t\t%d\t\t\t%d\n",emp[i].name,emp[i].age,emp[i].phno,emp[i].salary);
     }
